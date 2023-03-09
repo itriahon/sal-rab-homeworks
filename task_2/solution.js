1d0ed78efab95e25377c6a6a8d079e1453684a0a
Andrey Soldatenkov <logovas@list.ru>
2023-03-09 22:20:50 +0300

    task 2

--- task_2/solution.js
+++ task_2/solution.js
@@ -2,7 +2,15 @@ function calcShipping(sum, min, shipping) {
     let productsSum = sum; // сумма в корзине
     let freeShippingMinSum = min; // минимальная сумма для бесплатной доставки
     let shippingPrice = shipping; // стоимость доставки
-
+    let shippingSum;
+    
+    if (productsSum == 0) {
+        shippingSum = 0;
+    } else if (productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+    } else if (productsSum > 0 && productsSum < freeShippingMinSum) {
+        shippingSum = shippingPrice;
+    }   
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
@@ -25,7 +33,13 @@ function calcDiscount(sum, min, discount) {
     let productsSum = sum; // сумма в корзине
     let discountMinSum = min; // минимальная сумма для скидки
     let discountPart = discount; // величина скидки в процентах
+    let discountSum;
 
+    if (productsSum >= discountMinSum) {
+        discountSum = discountPart * productsSum / 100;
+    } else {
+        discountSum = 0;
+    }
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountSum
@@ -42,6 +56,9 @@ function calcDiscount(sum, min, discount) {
 function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shippingPrice}) {
     let productsSum = sum;
     let discountSum = calcDiscount(sum, discountMinSum, discountPart);
+    let totalSum = productsSum - discountSum;
+
+    
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
@@ -51,6 +68,12 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
+
+   
+    totalSum = totalSum + shippingSum;
+
+    let freeShipping;
+    freeShipping = !Boolean(shippingSum);
 
     // прибавьте к totalSum значение shippingSum
 
